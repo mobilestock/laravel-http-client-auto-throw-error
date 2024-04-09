2dd3988757e0b1144fdc01694bf5d6a9cd663e5f
formig-a <hrpdro2016@gmail.com>
2024-04-09 13:48:00 -0300

    reivew Gean: mudando nome de tabelas

--- apps/lookpay-api/app/Models/InvoicesItem.php
+++ apps/lookpay-api/app/Models/InvoicesItem.php
@@ -6,17 +6,17 @@ use App\Enum\Invoice\ItemTypeEnum;
 use Illuminate\Support\Carbon;
 
 /**
- * App\Models\InvoiceItem
+ * App\Models\InvoicesItem
  *
  * @property string $id
  * @property string $invoice_id
  * @property ItemTypeEnum $type
  * @property float $amount
- * @property ?Carbon $created_at
+ * @property Carbon $created_at
  */
-class InvoiceItem extends Model
+class InvoicesItem extends Model
 {
-    protected $table = 'invoices_items';
+    public $timestamps = false;
     protected $primaryKey = 'id';
     protected $fillable = ['id', 'invoice_id', 'type', 'amount'];
     protected $casts = ['type' => ItemTypeEnum::class];
