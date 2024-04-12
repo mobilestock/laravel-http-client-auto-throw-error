e9978361c022a5995202d11d5601370bb8c5cef0
geangontijo <geangontijo07@gmail.com>
2024-04-12 16:53:18 -0300

    tipagens

--- shared/pdo-cast/tests/TestCase.php
+++ shared/pdo-cast/tests/TestCase.php
@@ -1,7 +1,13 @@
 <?php
 
+use MobileStock\PdoCast\PdoCastStatement;
+use MobileStock\PdoCast\PdoCastStatement74;
+
 class TestCase extends PHPUnit\Framework\TestCase
 {
+    /**
+     * @return PdoCastStatement74|PdoCastStatement
+     */
     protected static function getStmt(...$args): PDOStatement
     {
         $reflectionClass = new ReflectionClass(MobileStock\PdoCast\StatementUtils::getStatementClass());
