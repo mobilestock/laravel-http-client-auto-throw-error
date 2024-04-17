9b543b2bc7384e7bceb3a165f68a464badfc51dd
geangontijo <geangontijo07@gmail.com>
2024-04-17 16:24:09 -0300

    redis e wc

--- apps/lookpay-api/app/Http/Controllers/InvoiceController.php
+++ apps/lookpay-api/app/Http/Controllers/InvoiceController.php
@@ -15,6 +15,7 @@ class InvoiceController
 {
     public function createInvoice()
     {
+        DB::beginTransaction();
         $dadosJson = Request::validate([
             'card' => ['required', 'array', 'min:6', 'max:6'],
             'card.number' => ['required', 'numeric', 'digits:16'],
@@ -24,7 +25,7 @@ class InvoiceController
             'card.month' => ['required', 'numeric', 'gte:0', 'lte:12'],
             'card.year' => ['required', 'numeric'],
             'method' => ['required', Rule::enum(PaymentMethodsEnum::class)],
-            'reference_id' => ['sometimes', 'required', 'max:26'],
+            'reference_id' => ['sometimes', 'required', 'max:26', 'unique:invoices,reference_id'],
             'items' => ['required', 'array', 'size:1'],
             'items.*.quantity' => ['required', 'numeric', 'gt:0'],
             'items.*.price_cents' => ['required', 'numeric', 'gt:0'],
@@ -32,7 +33,7 @@ class InvoiceController
             'months' => ['sometimes', 'required', 'numeric', 'gte:1'],
         ]);
         $fees = (array) json_decode(Auth::user()->fees);
-        $fee = ($fees[$dadosJson['months']] / 100) * $dadosJson['items'][0]['price_cents'];
+        $fee = ($fees[$dadosJson['months'] - 1] / 100) * $dadosJson['items'][0]['price_cents'];
 
         $invoice = new Invoice();
         $invoice->establishment_id = Auth::user()->id;
@@ -43,6 +44,7 @@ class InvoiceController
         if (!empty($dadosJson['reference_id'])) {
             $invoice->reference_id = $dadosJson['reference_id'];
         }
+
         $invoice->save();
 
         foreach ($dadosJson['items'] as $commission) {
