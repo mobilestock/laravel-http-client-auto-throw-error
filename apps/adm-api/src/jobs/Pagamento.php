5c9c96979332ec4b660a82322c6a909f3263468b
SavioAmin <87980098+SavioSReis@users.noreply.github.com>
2024-04-26 13:40:39 -0300

    Passando lógica pra dentro do evento

--- apps/adm-api/src/jobs/Pagamento.php
+++ apps/adm-api/src/jobs/Pagamento.php
@@ -6,11 +6,18 @@ use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Support\Facades\DB;
 use MobileStock\helper\Images\ImagemPagamentoAprovadoMobile;
+use MobileStock\model\Origem;
 use MobileStock\model\TransacaoFinanceira\TransacaoFinanceiraModel;
+use MobileStock\model\TrocaFilaSolicitacoesModel;
+use MobileStock\repository\TrocaPendenteRepository;
 use MobileStock\service\ColaboradoresService;
 use MobileStock\service\MessageService;
+use MobileStock\service\PedidoItem\PedidoItemMeuLookService;
 use MobileStock\service\ProdutoService;
 use MobileStock\service\TransacaoFinanceira\TransacaoFinanceiraService;
+use MobileStock\service\TransacaoFinanceira\TransacaoFinanceirasMetadadosService;
+use MobileStock\service\TransacaoFinanceira\TransacaoFinanceirasProdutosTrocasService;
+use MobileStock\service\TrocaFilaSolicitacoesService;
 use MobileStock\service\WebhookHttpClient;
 
 class Pagamento implements ShouldQueue
@@ -31,40 +38,78 @@ class Pagamento implements ShouldQueue
     ) {
         $transacao = (new TransacaoFinanceiraModel())->forceFill($this->dadosTransacao);
 
-        if ($transacao->origem_transacao === 'ML') {
-            $listaProdutos = ProdutoService::dadosMensagemPagamentoAprovado($transacao->id);
+        switch ($transacao->origem_transacao) {
+            case 'ML':
+                $listaProdutos = ProdutoService::dadosMensagemPagamentoAprovado($transacao->id);
 
-            $comMiniatura = count($listaProdutos) <= 50;
+                $comMiniatura = count($listaProdutos) <= 50;
 
-            $produtos = array_chunk($listaProdutos, 10);
+                $produtos = array_chunk($listaProdutos, 10);
 
-            foreach ($produtos as $produto) {
-                $listaProdutos = $produto;
-                $textoImagem = $imagemPagamentoAprovadoMobile->gerarImagem($listaProdutos, $comMiniatura);
-                $whatsapp->sendImageBase64WhatsApp(
-                    $listaProdutos[0]['telefone'],
-                    $textoImagem,
-                    'O pagamento do seu pedido Nº ' .
+                foreach ($produtos as $produto) {
+                    $listaProdutos = $produto;
+                    $textoImagem = $imagemPagamentoAprovadoMobile->gerarImagem($listaProdutos, $comMiniatura);
+                    $whatsapp->sendImageBase64WhatsApp(
+                        $listaProdutos[0]['telefone'],
+                        $textoImagem,
+                        'O pagamento do seu pedido Nº ' .
                         $listaProdutos[0]['id_transacao'] .
                         ' foi aprovado! Link para rastreio: ' .
                         $_ENV['URL_MEULOOK'] .
                         'usuario/historico'
+                    );
+                }
+                break;
+            case 'ZA':
+                $url = ColaboradoresService::consultaUrlWebhook($transacao->pagador);
+                $arrTransacao = TransacaoFinanceiraService::listaTransacoesApi(
+                    DB::getPdo(),
+                    $transacao->pagador,
+                    $transacao->id,
+                    1
                 );
-            }
-        } elseif ($transacao->origem_transacao === 'ZA') {
-            $url = ColaboradoresService::consultaUrlWebhook($transacao->pagador);
-            $arrTransacao = TransacaoFinanceiraService::listaTransacoesApi(
-                DB::getPdo(),
-                $transacao->pagador,
-                $transacao->id,
-                1
-            );
-            $arrInformacoes = [
-                'transacao' => $arrTransacao,
-                'evento' => 'transacao.paga',
-            ];
+                $arrInformacoes = [
+                    'transacao' => $arrTransacao,
+                    'evento' => 'transacao.paga',
+                ];
 
-            $httpClient->post($url, $arrInformacoes);
+                $httpClient->post($url, json_encode($arrInformacoes));
+                break;
+            case 'ET':
+                TransacaoFinanceirasProdutosTrocasService::converteDebitoPendenteParaNormalSeNecessario(
+                    $transacao->pagador
+                );
+                TransacaoFinanceirasProdutosTrocasService::sincronizaTrocaPendenteAgendamentoSeNecessario(
+                    $transacao->pagador
+                );
+                $uuids = (new TransacaoFinanceirasMetadadosService())->buscaUuidsMetadadoProdutosTroca($transacao->id);
+                if (empty($uuids)) {
+                    return;
+                }
+
+                foreach ($uuids as $uuidProduto) {
+                    $produto = PedidoItemMeuLookService::buscaDadosProdutoPorUuid(
+                        DB::getPdo(),
+                        $uuidProduto,
+                        Origem::ML
+                    );
+                    if ($produto['existe_agendamento']) {
+                        TrocaPendenteRepository::removeTrocaAgendadadaNormalMeuLook(DB::getPdo(), $uuidProduto);
+                    }
+                    if ($produto['id_solicitacao']) {
+                        $trocaFilaSolicitacoes = new TrocaFilaSolicitacoesModel();
+                        $trocaFilaSolicitacoes->exists = true;
+                        $trocaFilaSolicitacoes->id = $produto['id_solicitacao'];
+                        $trocaFilaSolicitacoes->situacao = TrocaFilaSolicitacoesModel::CANCELADO_PELO_CLIENTE;
+                        $trocaFilaSolicitacoes->update();
+                        TrocaFilaSolicitacoesService::enviarNotificacaoWhatsapp(
+                            DB::getPdo(),
+                            $produto['id_solicitacao'],
+                            Origem::ML
+                        );
+                    }
+                }
+                break;
         }
     }
 }
--- apps/adm-api/src/service/TransacaoFinanceira/TransacaoFinanceiraService.php
+++ apps/adm-api/src/service/TransacaoFinanceira/TransacaoFinanceiraService.php
@@ -16,7 +16,6 @@ use MobileStock\model\Lancamento;
 use MobileStock\model\LogisticaItemModel;
 use MobileStock\model\Origem;
 use MobileStock\model\TransacaoFinanceira\TransacaoFinanceira;
-use MobileStock\repository\TrocaPendenteRepository;
 use MobileStock\service\CancelamentoProdutos;
 use MobileStock\service\ColaboradoresService;
 use MobileStock\service\Iugu\IuguHttpClient;
@@ -25,7 +24,6 @@ use MobileStock\service\Lancamento\LancamentoService;
 use MobileStock\service\Pagamento\LancamentoPendenteService;
 use MobileStock\service\PedidoItem\PedidoItem;
 use MobileStock\service\PedidoItem\PedidoItemMeuLookService;
-use MobileStock\service\TrocaFilaSolicitacoesService;
 use PDO;
 use RuntimeException;
 use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
@@ -526,41 +524,16 @@ class TransacaoFinanceiraService extends TransacaoFinanceira
                 throw new RuntimeException('Quantidade inconsistente de lançamentos alterados.');
             }
         }
-
-        if (in_array($this->origem_transacao, ['ML', 'ZA'])) {
-            $job = new Pagamento([
-                'origem_transacao' => $this->origem_transacao,
-                'id' => $this->id,
-                'pagador' => $this->pagador,
-            ]);
-            dispatch($job->afterCommit());
-        } elseif ($this->origem_transacao === 'ET') {
-            $transacaoFinanceirasMetadadosService = new TransacaoFinanceirasMetadadosService();
-            $uuids = $transacaoFinanceirasMetadadosService->buscaUuidsMetadadoProdutosTroca($this->id);
-            if (!$uuids) {
-                return;
-            }
-            $uuids = json_decode($uuids);
-            foreach ($uuids as $uuid) {
-                $dadosProduto = PedidoItemMeuLookService::buscaDadosProdutoPorUuid($conexao, $uuid, 'ML');
-
-                if ($dadosProduto['existe_agendamento']) {
-                    TrocaPendenteRepository::removeTrocaAgendadadaNormalMeuLook($conexao, $uuid);
-                }
-
-                if ($dadosProduto['id_solicitacao']) {
-                    $model = new TrocaFilaSolicitacoesService();
-                    $model->id = $dadosProduto['id_solicitacao'];
-                    $model->situacao = 'CANCELADO_PELO_CLIENTE';
-                    $model->atualizar($conexao);
-                    TrocaFilaSolicitacoesService::enviarNotificacaoWhatsapp(
-                        $conexao,
-                        $dadosProduto['id_solicitacao'],
-                        'ML'
-                    );
-                }
-            }
+        if (!in_array($this->origem_transacao, ['ML', 'ZA', 'ET'])) {
+            return;
         }
+
+        $job = new Pagamento([
+            'origem_transacao' => $this->origem_transacao,
+            'id' => $this->id,
+            'pagador' => $this->pagador,
+        ]);
+        dispatch($job->afterCommit());
     }
 
     //    public function insereProdutoPagoPainel(PDO $conn, int $id_produto, int $idUsuario, array $grade): void
@@ -933,7 +906,7 @@ class TransacaoFinanceiraService extends TransacaoFinanceira
     }
 
     /**
-     * https://github.com/mobilestock/backend/issues/124
+     * https://github.com/mobilestock/web/issues/3067
      */
     public function consultaTransacaoCancelamento(): void
     {
