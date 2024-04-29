1f99178f7328b89d6b17cce4368d6164cb4d40ce
SavioAmin <87980098+SavioSReis@users.noreply.github.com>
2024-04-29 10:57:02 -0300

    Evento consulta do model e faz a lógica dele

--- apps/adm-api/src/jobs/jobSeparacaoAtrasada.php
+++ apps/adm-api/src/jobs/jobSeparacaoAtrasada.php
@@ -3,14 +3,27 @@
 namespace MobileStock\jobs;
 
 use MobileStock\jobs\config\AbstractJob;
-use MobileStock\service\Conferencia\ConferenciaService;
+use MobileStock\model\LogisticaItemModel;
+use MobileStock\service\MessageService;
 
 require_once __DIR__ . '/../../vendor/autoload.php';
 
-return new class extends AbstractJob
-{
-    public function run(\PDO $conexao)
+return new class extends AbstractJob {
+    public function run()
     {
-        ConferenciaService::enviaMensagemParaSellerComSeparacaoAtrasada($conexao);
+        $produtosAtrasados = LogisticaItemModel::buscaProdutosComConferenciaAtrasada();
+        if (empty($produtosAtrasados)) {
+            return;
+        }
+
+        $msgService = app(MessageService::class);
+        foreach ($produtosAtrasados as $produto) {
+            $mensagem = "Produto *{$produto['id_produto']}* - *{$produto['nome_tamanho']}* atrasado!";
+            $mensagem .= PHP_EOL . PHP_EOL;
+            $mensagem .= 'Caso não seja separado, sua venda será cancelada no próximo dia útil. ';
+            $mensagem .= 'Caso não tenha o produto em estoque, você pode sugerir a substituição por outro produto ';
+            $mensagem .= 'semelhante ao cliente.';
+            $msgService->sendImageWhatsApp($produto['telefone'], $produto['foto_produto'], $mensagem);
+        }
     }
 };
