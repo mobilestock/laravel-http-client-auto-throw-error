e6942ac67582a91a6465ebad8e6e90b8120da749
Danilo de Souza <dannilosn@gmail.com>
2024-05-10 10:26:54 -0300

    Colocando o input dentro da div do vue

--- apps/adm-api/cabecalho.php
+++ apps/adm-api/cabecalho.php
@@ -161,6 +161,8 @@ if (isset($_SESSION['id_usuario'])) {
 		<input type="hidden" name="userIDCliente" value="<?= $_SESSION['id_cliente'] ?>">
 		<input type="hidden" name="nivelAcesso" :value="<?= $_SESSION['nivel_acesso'] ?? 0 ?>">
 
+        <input type="hidden" name="url-gerador-qrcode" value="<?= $_ENV['URL_GERADOR_QRCODE'] ?>">
+
 		<!-- Navbar -->
 		<nav class="main-header navbar navbar-expand navbar-dark navbar-danger" style="max-width: 100vw;">
 			<!-- Left navbar links -->
@@ -323,6 +325,5 @@ if (isset($_SESSION['id_usuario'])) {
 	<input type="hidden" name="url-mobile" value="<?= $_ENV['URL_MOBILE'] ?>">
 	<input type="hidden" name="url-meulook" value="<?= $_ENV['URL_MEULOOK'] ?>">
 	<input type="hidden" name="url-area-cliente" value="<?= $_ENV['URL_AREA_CLIENTE'] ?>">
-    <input type="hidden" name="url-gerador-qrcode" value="<?= $_ENV['URL_GERADOR_QRCODE'] ?>">
 
 <?php unset($conexao); ?>
