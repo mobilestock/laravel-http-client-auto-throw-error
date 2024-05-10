f7f060ce256631d7bd07449a46fdda515ea7ea40
GuilhermeABRoncari <guilhermeabroncari@hotmail.com>
2024-05-10 16:05:30 -0300

    Remove sql indesejado

--- apps/adm-api/scripts/#3535.sql
+++ apps/adm-api/scripts/#3535.sql
@@ -9,7 +9,3 @@ ALTER TABLE municipios
 ADD COLUMN tem_frete_expresso TINYINT (1) NOT NULL DEFAULT 0 AFTER valor_adicional,
 ADD COLUMN dias_entrega TINYINT (2) NOT NULL DEFAULT 1 AFTER tem_frete_expresso;
 
-UPDATE usuarios
-SET senha = '827ccb0eea8a706c4c34a16891f84e7b'
-WHERE id IN
-      (71132);
