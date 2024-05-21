ec9081408b8a15435c87cf719fce648f236ec732
GuilhermeABRoncari <guilhermeabroncari@hotmail.com>
2024-05-21 11:04:52 -0300

    Faz endentação do script

--- apps/adm-api/scripts/#3536.sql
+++ apps/adm-api/scripts/#3536.sql
@@ -1,16 +1,25 @@
 ALTER TABLE produtos ADD COLUMN eh_moda TINYINT NOT NULL DEFAULT 0;
-ALTER TABLE catalogo_fixo MODIFY COLUMN tipo ENUM(
-    'IMPULSIONAR', 
-    'MELHOR_FABRICANTE', 
-    'PROMOCAO_TEMPORARIA', 
-    'VENDA_RECENTE', 
-    'MELHOR_PONTUACAO', 
-    'MODA_GERAL', 
+
+ALTER TABLE catalogo_fixo
+MODIFY COLUMN tipo ENUM(
+    'IMPULSIONAR',
+    'MELHOR_FABRICANTE',
+    'PROMOCAO_TEMPORARIA',
+    'VENDA_RECENTE',
+    'MELHOR_PONTUACAO',
+    'MODA_GERAL',
     'MODA_20',
     'MODA_40',
     'MODA_60',
     'MODA_80',
     'MODA_100'
-    );
-ALTER TABLE colaboradores ADD COLUMN porcentagem_compras_moda TINYINT NOT NULL DEFAULT 0;
-UPDATE produtos SET produtos.eh_moda = 1 WHERE produtos.id_fornecedor IN (12, 6984);
+);
+
+ALTER TABLE colaboradores
+ADD COLUMN porcentagem_compras_moda TINYINT NOT NULL DEFAULT 0;
+
+UPDATE produtos
+SET
+    produtos.eh_moda = 1
+WHERE
+    produtos.id_fornecedor IN (12, 6984);
