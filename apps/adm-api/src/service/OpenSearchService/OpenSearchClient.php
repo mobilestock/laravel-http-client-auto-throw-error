c34471eb2e99b9dcef812b88dc3ec539e1938a7d
Danilo de Souza <dannilosn@gmail.com>
2024-05-21 16:55:29 -0300

    Verificar se produto tem estoque usando match à regex na grade teve uma melhora de 49%

--- apps/adm-api/src/service/OpenSearchService/OpenSearchClient.php
+++ apps/adm-api/src/service/OpenSearchService/OpenSearchClient.php
@@ -46,8 +46,10 @@ class OpenSearchClient extends HttpClient
         $opcional = [];
 
         $grade = 'grade_fullfillment';
+        $chave_tem_estoque = 'tem_fullfillment';
         if ($origem === 'ML' && $estoque !== 'FULLFILLMENT') {
             $grade = 'grade_produto';
+            $chave_tem_estoque = 'tem_estoque';
         }
 
         $chaveValor = 'valor_venda_ms';
@@ -127,7 +129,7 @@ class OpenSearchClient extends HttpClient
             if (!empty($tamanhos)) {
                 $obrigatorio[] = ['match' => [$grade => ['query' => implode('|', $tamanhos), 'boost' => 0]]];
             } else {
-                $obrigatorio[] = ['regexp' => [$grade => '.+']];
+                $obrigatorio[] = ['match' => [$chave_tem_estoque => 1]];
             }
             if (!empty($cores)) {
                 $obrigatorio[] = ['match' => ['cor_produto' => ['query' => implode('|', $cores), 'boost' => 0]]];
