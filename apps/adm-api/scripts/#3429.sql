d9c81e384aec4c643e1a82b193e2ad13df0fa382
Danilo de Souza <dannilosn@gmail.com>
2024-05-22 16:59:33 -0300

    Script atualizado para usuário ter permissão em tabelas específicas

--- apps/adm-api/scripts/#3429.sql
+++ apps/adm-api/scripts/#3429.sql
@@ -1,4 +1,4 @@
-# Para essa tarefa precisaremos de um usuário novo só com permissão de SELECT no banco de dados.
+# Para essa tarefa precisaremos de um usuário novo só com permissão de SELECT em tabelas específicas do banco de dados.
 
 # Antes de criá-lo busque online um gerador de senhas seguro e crie uma senha com:
 #   Caracteres especiais,
@@ -8,9 +8,13 @@
 
 # Quando o fizer substitua na query o termo senha-segura pela senha gerada e rode o script.
 
-CREATE USER 'select_only'@'%' IDENTIFIED BY 'senha-segura';
-GRANT SELECT ON mobile_stock.* TO 'select_only'@'%';
+CREATE USER 'user_read_only'@'%' IDENTIFIED BY 'senha-segura';
+GRANT SELECT ON mobile_stock.colaboradores_enderecos_logs TO 'user_read_only'@'%';
+GRANT SELECT ON mobile_stock.entregas_logs TO 'user_read_only'@'%';
+GRANT SELECT ON mobile_stock.logistica_item_logs TO 'user_read_only'@'%';
+GRANT SELECT ON mobile_stock.pedido_item_logs TO 'user_read_only'@'%';
+GRANT SELECT ON mobile_stock.transacao_financeiras_logs TO 'user_read_only'@'%';
 FLUSH PRIVILEGES;
 
-# O script criará um usuário com o nome 'select_only' e senha que precisarão serem serem adicionado ao .env do projeto na produção,
+# O script criará um usuário com o nome 'user_read_only' e senha que precisarão serem serem adicionado ao .env do projeto na produção,
 # verificar .env.example para saber as chaves.
