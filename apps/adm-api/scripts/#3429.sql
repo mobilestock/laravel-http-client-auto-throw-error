f52bd311c588f0fd5d79ffd051ae7f5d746e3ba6
Danilo de Souza <dannilosn@gmail.com>
2024-05-22 17:35:34 -0300

    Removendo tabelas a pedido do @geangontijo

--- apps/adm-api/scripts/#3429.sql
+++ apps/adm-api/scripts/#3429.sql
@@ -10,16 +10,9 @@
 
 CREATE USER 'user_read_only'@'%' IDENTIFIED BY 'senha-segura';
 GRANT SELECT ON mobile_stock.colaboradores_enderecos_logs TO 'user_read_only'@'%';
-GRANT SELECT ON mobile_stock.colaboradores_log TO 'user_read_only'@'%';
-GRANT SELECT ON mobile_stock.entregas_log_devolucoes_item TO 'user_read_only'@'%';
-GRANT SELECT ON mobile_stock.entregas_log_faturamento_item TO 'user_read_only'@'%';
-GRANT SELECT ON mobile_stock.entregas_logs TO 'user_read_only'@'%';
 GRANT SELECT ON mobile_stock.logistica_item_logs TO 'user_read_only'@'%';
 GRANT SELECT ON mobile_stock.negociacoes_produto_log TO 'user_read_only'@'%';
 GRANT SELECT ON mobile_stock.pedido_item_logs TO 'user_read_only'@'%';
-GRANT SELECT ON mobile_stock.pontos_coleta_calculo_percentual_frete_logs TO 'user_read_only'@'%';
-GRANT SELECT ON mobile_stock.tipo_frete_log TO 'user_read_only'@'%';
-GRANT SELECT ON mobile_stock.transacao_financeiras_logs TO 'user_read_only'@'%';
 FLUSH PRIVILEGES;
 
 # O script criará um usuário com o nome 'user_read_only' e senha que precisarão serem serem adicionado ao .env do projeto na produção,
