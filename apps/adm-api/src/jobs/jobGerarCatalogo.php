25563cd822f374786470096b88c8d076982ff4dc
GuilhermeABRoncari <guilhermeabroncari@hotmail.com>
2024-06-06 11:11:34 -0300

    Unifica os jobs de catalogo

--- apps/adm-api/src/jobs/jobGerarCatalogo.php
+++ apps/adm-api/src/jobs/jobGerarCatalogo.php
@@ -2,29 +2,27 @@
 
 namespace MobileStock\jobs;
 
-use Exception;
 use MobileStock\jobs\config\AbstractJob;
 use MobileStock\service\CatalogoFixoService;
 use MobileStock\repository\ProdutosRepository;
 
 require_once __DIR__ . '/../../vendor/autoload.php';
 
-return new class extends AbstractJob
-{
+return new class extends AbstractJob {
     public function run(\PDO $conexao)
     {
-        try {
-            $conexao->beginTransaction();
-            // CatalogoFixoService::geraMelhoresFabricantes($conexao);
-            CatalogoFixoService::removeItensInvalidos($conexao);
-            ProdutosRepository::limparUltimosAcessos($conexao);
-            CatalogoFixoService::atualizaInformacoesProdutosCatalogoFixo($conexao);
-            CatalogoFixoService::geraVendidosRecentemente();
-            CatalogoFixoService::geraMelhoresProdutos($conexao);
-            $conexao->commit();
-        } catch (Exception $exception) {
-            $conexao->rollBack();
-            throw $exception;
+        $conexao->beginTransaction();
+        CatalogoFixoService::removeItensInvalidos($conexao);
+        ProdutosRepository::limparUltimosAcessos($conexao);
+        CatalogoFixoService::atualizaInformacoesProdutosCatalogoFixo($conexao);
+        CatalogoFixoService::geraVendidosRecentemente();
+        CatalogoFixoService::geraMelhoresProdutos($conexao);
+        CatalogoFixoService::geraCatalogoModaComPorcentagem(CatalogoFixoService::TIPO_MODA_GERAL);
+
+        for ($porcentagem = 20; $porcentagem <= 100; $porcentagem += 20) {
+            $tag = 'MODA_' . $porcentagem;
+            CatalogoFixoService::geraCatalogoModaComPorcentagem($tag, $porcentagem);
         }
+        $conexao->commit();
     }
 };
