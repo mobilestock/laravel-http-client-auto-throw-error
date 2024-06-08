f4d1cdfc1c88a5d4f3f53b1b86d5f639c51ae51d
Gustavo Aurélio <44369818+Gustavo210@users.noreply.github.com>
2024-06-08 12:53:25 -0300

    nulo para 0

--- apps/adm-api/src/service/CatalogoPersonalizadoService.php
+++ apps/adm-api/src/service/CatalogoPersonalizadoService.php
@@ -168,7 +168,7 @@ class CatalogoPersonalizadoService extends CatalogoPersonalizado
             $select .= ",
                 LOWER(IF(LENGTH(produtos.nome_comercial) > 0, produtos.nome_comercial, produtos.descricao)) `nome_produto`,
                 $chaveValor `valor_venda`,
-                IF (produtos.promocao > 0, $chaveValorHistorico, NULL) `valor_venda_historico`,
+                IF (produtos.promocao > 0, $chaveValorHistorico, 0) `valor_venda_historico`,
                 reputacao_fornecedores.reputacao,
                 produtos.quantidade_vendida";
             $join .=
