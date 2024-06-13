e9602f981aa2a3a13be8b7d7d0fa59972a9949be
GuilhermeABRoncari <guilhermeabroncari@hotmail.com>
2024-06-13 13:57:02 -0300

    Arruma alias

--- apps/adm-api/src/model/AcompanhamentoTemp.php
+++ apps/adm-api/src/model/AcompanhamentoTemp.php
@@ -181,7 +181,7 @@ class AcompanhamentoTemp extends Model
                     IF (
                         tipo_frete.id = 2, 'ENVIO_TRANSPORTADORA', tipo_frete.tipo_ponto
                     ) AS `tipo_ponto`,
-                    tipo_frete.id IN ($idTipoFreteEntregaCliente) AS `bool_entrega_cliente`,
+                    tipo_frete.id IN ($idTipoFreteEntregaCliente) AS `eh_entrega_cliente`,
                     (
                         SELECT CONCAT(municipios.nome, ' (', municipios.uf, ')')
                         FROM municipios
@@ -232,7 +232,7 @@ class AcompanhamentoTemp extends Model
         $query = "SELECT
                         logistica_item.id_entrega AS `id_entrega`,
                         DATE_FORMAT(logistica_item.data_atualizacao, '%d/%m/%Y %H:%i:%s') AS `data_criacao`,
-                        @entrega_cliente := tipo_frete.id IN ($idEntregaCliente) AS `eh_entrega_cliente`,
+                        @entrega_cliente := tipo_frete.id IN ($idEntregaCliente) AS `bool_entrega_cliente`,
                         CASE
                             WHEN @entrega_cliente = TRUE THEN (
                                 SELECT JSON_OBJECT(
