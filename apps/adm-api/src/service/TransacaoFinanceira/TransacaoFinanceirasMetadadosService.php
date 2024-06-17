c43c3c2fd144bf28945016af0634f1047dc69664
Guilherme Alves Batista Roncari <guilhermeabroncari@hotmail.com>
2024-06-17 08:52:24 -0300

    Update apps/adm-api/src/service/TransacaoFinanceira/TransacaoFinanceirasMetadadosService.php
    Co-authored-by: SavioAmin <87980098+SavioSReis@users.noreply.github.com>

--- apps/adm-api/src/service/TransacaoFinanceira/TransacaoFinanceirasMetadadosService.php
+++ apps/adm-api/src/service/TransacaoFinanceira/TransacaoFinanceirasMetadadosService.php
@@ -184,32 +184,24 @@ class TransacaoFinanceirasMetadadosService extends TransacaoFinanceirasMetadados
         return $uuids;
     }
 
-    public static function buscaColaboradoresColetasAnteriores(): ?array
+    public static function buscaColaboradoresColetasAnteriores(): array
     {
         $sql = "SELECT
-                    GROUP_CONCAT(
-                        DISTINCT JSON_OBJECT(
-                            'id_colaborador', colaboradores_enderecos.id_colaborador,
-                            'razao_social', JSON_EXTRACT(transacao_financeiras_metadados.valor, '$.nome_destinatario'),
-                            'telefone', JSON_EXTRACT(transacao_financeiras_metadados.valor, '$.telefone_destinatario'),
-                            'id_endereco', JSON_EXTRACT(transacao_financeiras_metadados.valor, '$.id'),
-                            'logradouro', JSON_EXTRACT(transacao_financeiras_metadados.valor, '$.logradouro'),
-                            'numero', JSON_EXTRACT(transacao_financeiras_metadados.valor, '$.numero'),
-                            'bairro', JSON_EXTRACT(transacao_financeiras_metadados.valor, '$.bairro'),
-                            'cidade', JSON_EXTRACT(transacao_financeiras_metadados.valor, '$.cidade'),
-                            'uf', JSON_EXTRACT(transacao_financeiras_metadados.valor, '$.uf')
-                        )
-                    ) AS json_coletas_anteriores
-                FROM transacao_financeiras_metadados
-                INNER JOIN transacao_financeiras ON transacao_financeiras.id = transacao_financeiras_metadados.id_transacao
-                    AND transacao_financeiras.pagador = :id_cliente
-                INNER JOIN colaboradores_enderecos ON colaboradores_enderecos.id = JSON_VALUE(transacao_financeiras_metadados.valor, '$.id')
-                WHERE transacao_financeiras_metadados.chave = 'ENDERECO_COLETA_JSON'
-                GROUP BY colaboradores_enderecos.id_colaborador
+                    JSON_VALUE(transacao_financeiras_metadados.valor, '$.id_colaborador') AS `id_colaborador`,
+                    JSON_VALUE(transacao_financeiras_metadados.valor, '$.nome_destinatario') AS `razao_social`,
+                    JSON_VALUE(transacao_financeiras_metadados.valor, '$.telefone_destinatario') AS `telefone`,
+                    colaboradores.foto_perfil
+                FROM transacao_financeiras
+                INNER JOIN transacao_financeiras_metadados ON transacao_financeiras_metadados.chave = 'ENDERECO_COLETA_JSON'
+                    AND transacao_financeiras_metadados.id_transacao = transacao_financeiras.id
+                INNER JOIN colaboradores ON colaboradores.id = JSON_EXTRACT(transacao_financeiras_metadados.valor, '$.id_colaborador')
+                WHERE transacao_financeiras.pagador = :id_cliente
+                GROUP BY JSON_VALUE(transacao_financeiras_metadados.valor, '$.id_colaborador')
                 ORDER BY transacao_financeiras_metadados.id DESC
                 LIMIT 10";
 
-        $colaboradoresAnteriores = DB::selectColumns($sql, ['id_cliente' => Auth::user()->id_colaborador]);
+        $colaboradoresAnteriores = DB::select($sql, ['id_cliente' => Auth::user()->id_colaborador]);
+
         return $colaboradoresAnteriores;
     }
 
