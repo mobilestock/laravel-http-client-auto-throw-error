e33ebebfc0f7a4c10ad8e4e8e035a661e43a573a
felprangel <feliperangel897@gmail.com>
2024-06-20 11:14:10 -0300

    altera retorno para self

--- apps/adm-api/src/model/ProdutosVideo.php
+++ apps/adm-api/src/model/ProdutosVideo.php
@@ -16,15 +16,15 @@ class ProdutosVideo extends Model
 {
     protected $fillable = ['id_produto', 'id_usuario', 'link'];
 
-    public static function buscaProdutoVideoPorLink(string $link, int $idProduto): ?int
+    public static function buscaProdutoVideoPorLink(string $link, int $idProduto): ?self
     {
-        $produtoVideo = DB::selectOneColumn(
+        $produtoVideo = self::fromQuery(
             'SELECT produtos_videos.id
             FROM produtos_videos
             WHERE produtos_videos.link = :link
                 AND produtos_videos.id_produto = :id_produto',
             ['link' => $link, 'id_produto' => $idProduto]
-        );
+        )->first();
         return $produtoVideo;
     }
 }
