550dbb524ad1b9d36f312f917e2338a249fef22c
SavioAmin <87980098+SavioSReis@users.noreply.github.com>
2024-06-20 15:06:33 -0300

    Não é nullable

--- apps/lookpay-api/database/migrations/2024_06_04_100032_3584.php
+++ apps/lookpay-api/database/migrations/2024_06_04_100032_3584.php
@@ -8,7 +8,7 @@ return new class extends Migration {
     public function up(): void
     {
         Schema::table('invoices', function (Blueprint $table) {
-            $table->string('establishment_order_id')->nullable()->default(null)->change();
+            $table->string('establishment_order_id', 100)->nullable(false)->change();
             $table->dropUnique('invoices_establishment_order_id_unique');
         });
     }
