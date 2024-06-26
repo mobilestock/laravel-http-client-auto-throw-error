075c22393dea45754287bfe0f6a0340459868c1c
felprangel <feliperangel897@gmail.com>
2024-06-26 10:49:44 -0300

    altera nome das colunas para usar cast de json

--- apps/adm-api/scripts/#3530.sql
+++ apps/adm-api/scripts/#3530.sql
@@ -1,2 +1,4 @@
 ALTER TABLE catalogo_personalizado
-    CHANGE COLUMN catalogo_personalizado.ativo catalogo_personalizado.esta_ativo TINYINT(1) DEFAULT 1;
+    CHANGE COLUMN catalogo_personalizado.ativo catalogo_personalizado.esta_ativo TINYINT(1) DEFAULT 1,
+    CHANGE COLUMN catalogo_personalizado.produtos catalogo_personalizado.json_produtos TEXT NOT NULL DEFAULT '[]',
+    CHANGE COLUMN catalogo_personalizado.plataformas_filtros catalogo_personalizado.json_plataformas_filtros VARCHAR(20) DEFAULT '["MS","ML","MED"]';
