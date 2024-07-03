af755d6a1213abd6c4eb556f331e7b36d91156bb
GuilhermeABRoncari <guilhermeabroncari@hotmail.com>
2024-07-03 11:33:12 -0300

    Modela classes ORM

--- apps/adm-api/src/model/Reposicao.php
+++ apps/adm-api/src/model/Reposicao.php
@@ -2,7 +2,15 @@
 
 namespace MobileStock\model;
 
+/**
+ * @property int $id
+ * @property int $id_fornecedor
+ * @property string $data_previsao
+ * @property int $id_usuario
+ * @property string $situacao
+ */
 class Reposicao extends Model
 {
     protected $table = 'reposicoes';
+    protected $fillable = ['id_fornecedor', 'data_previsao', 'id_usuario', 'situacao'];
 }
--- apps/adm-api/src/model/ReposicaoGrade.php
+++ apps/adm-api/src/model/ReposicaoGrade.php
@@ -2,7 +2,35 @@
 
 namespace MobileStock\model;
 
+/**
+ * @property int $id
+ * @property int $id_reposicao
+ * @property int $id_produto
+ * @property string $nome_tamanho
+ * @property float $preco_custo_produto
+ * @property int $quantidade_entrada
+ * @property int $quantidade_total
+ * @property int $id_usuario
+ * @property string $data_alteracao
+ */
 class ReposicaoGrade extends Model
 {
-    protected $table = 'reposicoes_grade';
+    protected $table = 'reposicoes_grades';
+    protected $fillable = [
+        'id_reposicao',
+        'id_produto',
+        'nome_tamanho',
+        'preco_custo_produto',
+        'quantidade_entrada',
+        'quantidade_total',
+        'id_usuario',
+        'data_alteracao',
+    ];
+    public $timestamps = false;
+
+    public static function sqlCalculoPrecoTotalReposicao(): string
+    {
+        $sql = 'SUM(reposicoes_grades.preco_custo_produto * reposicoes_grades.quantidade_entrada)';
+        return $sql;
+    }
 }
