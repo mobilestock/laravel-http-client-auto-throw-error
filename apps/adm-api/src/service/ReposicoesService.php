8e4ab83b7bf35c6823a11b88d3b17bbe39e85fff
GuilhermeABRoncari <guilhermeabroncari@hotmail.com>
2024-07-04 13:28:17 -0300

    Altera forma de verificação

--- apps/adm-api/src/service/ReposicoesService.php
+++ apps/adm-api/src/service/ReposicoesService.php
@@ -167,29 +167,25 @@ class ReposicoesService
                 CONCAT(
                     '[',
                         GROUP_CONCAT(DISTINCT
-                            IF(
-                                (reposicoes_grades.quantidade_total - reposicoes_grades.quantidade_entrada) > 0,
-                                JSON_OBJECT(
-                                    'id_reposicao', reposicoes.id,
-                                    'id_grade', reposicoes_grades.id,
-                                    'id_produto', reposicoes_grades.id_produto,
-                                    'cod_barras', (
-                                        SELECT produtos_grade.cod_barras
-                                        FROM produtos_grade
-                                        WHERE produtos_grade.id_produto = reposicoes_grades.id_produto
-                                            AND produtos_grade.nome_tamanho = reposicoes_grades.nome_tamanho
-                                        LIMIT 1
-                                    ),
-                                    'referencia', (
-                                        SELECT CONCAT(produtos.descricao, ' ', produtos.cores)
-                                        FROM produtos
-                                        WHERE produtos.id = reposicoes_grades.id_produto
-                                        LIMIT 1
-                                    ),
-                                    'quantidade_falta_entrar', reposicoes_grades.quantidade_total - reposicoes_grades.quantidade_entrada,
-                                    'nome_tamanho', reposicoes_grades.nome_tamanho
+                            JSON_OBJECT(
+                                'id_reposicao', reposicoes.id,
+                                'id_grade', reposicoes_grades.id,
+                                'id_produto', reposicoes_grades.id_produto,
+                                'cod_barras', (
+                                    SELECT produtos_grade.cod_barras
+                                    FROM produtos_grade
+                                    WHERE produtos_grade.id_produto = reposicoes_grades.id_produto
+                                        AND produtos_grade.nome_tamanho = reposicoes_grades.nome_tamanho
+                                    LIMIT 1
                                 ),
-                                NULL
+                                'referencia', (
+                                    SELECT CONCAT(produtos.descricao, ' ', produtos.cores)
+                                    FROM produtos
+                                    WHERE produtos.id = reposicoes_grades.id_produto
+                                    LIMIT 1
+                                ),
+                                'quantidade_falta_entrar', reposicoes_grades.quantidade_total - reposicoes_grades.quantidade_entrada,
+                                'nome_tamanho', reposicoes_grades.nome_tamanho
                             ) ORDER BY reposicoes_grades.nome_tamanho ASC
                         ),
                     ']'
@@ -198,6 +194,7 @@ class ReposicoesService
             INNER JOIN reposicoes_grades ON reposicoes_grades.id_reposicao = reposicoes.id
             WHERE reposicoes_grades.id_produto = :id_produto
                 AND reposicoes.situacao IN ('EM_ABERTO', 'PARCIALMENTE_ENTREGUE')
+                AND (reposicoes_grades.quantidade_total - reposicoes_grades.quantidade_entrada) > 0
             GROUP BY reposicoes.id
             ORDER BY reposicoes.id DESC",
             ['id_produto' => $idProduto]
