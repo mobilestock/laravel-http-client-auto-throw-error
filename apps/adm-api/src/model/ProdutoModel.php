488dd071b3f10d2201656665cd32d016af6a311b
Danilo de Souza <dannilosn@gmail.com>
2024-07-08 12:55:48 -0300

    # Commit 1 - Query de produtos parado retorna se o produto está em promoção - Usando valor de dias_baixar_preco em dias_parado - Se a promoção estiver ativa, usa a função legada para desativá-la e manter o preco_custo do produto

--- apps/adm-api/src/model/ProdutoModel.php
+++ apps/adm-api/src/model/ProdutoModel.php
@@ -71,6 +71,7 @@ class ProdutoModel extends Model
             DATE_FORMAT(_log_estoque_movimentacao.data, '%d/%m/%Y %H:%i') AS `data_ultima_entrada`,
             colaboradores.telefone,
             produtos.valor_custo_produto,
+            produtos.promocao > 0 AS `esta_em_promocao`,
             DATE(GREATEST(
                 COALESCE(_logistica_item.data, 0),
                 _log_estoque_movimentacao.data
@@ -78,6 +79,7 @@ class ProdutoModel extends Model
         $whereIdsExistentes = '';
 
         if ($catalogo) {
+            $binds['dias_parado'] = $binds['dias_baixar_preco'];
             unset($binds['dias_baixar_preco']);
             $select = ",
                 produtos.nome_comercial AS `nome_produto`,
