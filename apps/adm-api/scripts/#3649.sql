80121911444381da8abfc46a15775d773d8257c3
SavioAmin <87980098+SavioSReis@users.noreply.github.com>
2024-07-12 17:35:36 -0300

    Criei o script que cria a nova coluna

---
+++ apps/adm-api/scripts/#3649.sql
@@ -0,0 +1,7 @@
+ALTER TABLE `configuracoes`
+    DROP COLUMN `horarios_separacao_fulfillment`,
+	ADD COLUMN `json_logistica` LONGTEXT NOT NULL DEFAULT '{}' AFTER `minutos_expiracao_cache_filtros`,
+	ADD CONSTRAINT `json_logistica` CHECK (json_valid(`json_logistica`));
+
+UPDATE configuracoes
+    SET configuracoes.json_logistica = '{"separacao_fulfillment": {"horarios": ["08:00", "14:00"], "horas_carencia_retirada": "02:30"}}';
