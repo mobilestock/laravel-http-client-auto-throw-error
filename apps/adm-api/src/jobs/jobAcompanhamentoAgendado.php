24664783427f2987e097675ae8d6c39bb5705f48
SavioAmin <87980098+SavioSReis@users.noreply.github.com>
2024-07-15 10:28:02 -0300

    Mudando pra facades

--- apps/adm-api/src/jobs/jobAcompanhamentoAgendado.php
+++ apps/adm-api/src/jobs/jobAcompanhamentoAgendado.php
@@ -3,6 +3,7 @@
 namespace MobileStock\jobs;
 
 use DateTime;
+use Illuminate\Support\Facades\DB;
 use MobileStock\helper\Globals;
 use MobileStock\jobs\config\AbstractJob;
 use MobileStock\service\AcompanhamentoTempService;
@@ -16,40 +17,34 @@ require_once __DIR__ . '/../../vendor/autoload.php';
 
 return new class extends AbstractJob {
     public function run(
-        PDO $conexao,
         PrevisaoService $previsao,
         PontosColetaAgendaAcompanhamentoService $agenda,
         AcompanhamentoTempService $acompanhamento
     ) {
-        try {
-            $conexao->beginTransaction();
-            $dataAtual = new DateTime('NOW');
-            $ehDiaUtil = DiaUtilService::ehDiaUtil($dataAtual->format('Y-m-d'));
-            if (!$ehDiaUtil) {
-                return;
-            }
-
-            $IDXSemana = (int) $dataAtual->format('N');
-            $diasSemana = Globals::DIAS_SEMANA;
-            $diaAtual = $diasSemana[$IDXSemana];
-            $horarioSeparacao = $previsao->buscaHorarioSeparando();
-            $pontosColeta = $agenda->buscaPontosColetaAgendados($diaAtual, $horarioSeparacao);
-            if (empty($pontosColeta)) {
-                return;
-            }
-
-            $produtos = $acompanhamento->buscaProdutosParaAdicionarNoAcompanhamentoPorPontosColeta($pontosColeta);
-            if (empty($produtos)) {
-                return;
-            }
-
-            $agenda->removeHorariosPontuais($diaAtual, $horarioSeparacao);
-
-            $conexao->commit();
-            dispatch(new GerenciarAcompanhamento($produtos, GerenciarAcompanhamento::CRIAR_ACOMPANHAMENTO));
-        } catch (Throwable $th) {
-            $conexao->rollBack();
-            throw $th;
+        DB::beginTransaction();
+        $dataAtual = new DateTime('NOW');
+        $ehDiaUtil = DiaUtilService::ehDiaUtil($dataAtual->format('Y-m-d'));
+        if (!$ehDiaUtil) {
+            return;
         }
+
+        $IDXSemana = (int) $dataAtual->format('N');
+        $diasSemana = Globals::DIAS_SEMANA;
+        $diaAtual = $diasSemana[$IDXSemana];
+        $horarioSeparacao = $previsao->buscaHorarioSeparando();
+        $pontosColeta = $agenda->buscaPontosColetaAgendados($diaAtual, $horarioSeparacao);
+        if (empty($pontosColeta)) {
+            return;
+        }
+
+        $produtos = $acompanhamento->buscaProdutosParaAdicionarNoAcompanhamentoPorPontosColeta($pontosColeta);
+        if (empty($produtos)) {
+            return;
+        }
+
+        $agenda->removeHorariosPontuais($diaAtual, $horarioSeparacao);
+
+        DB::commit();
+        dispatch(new GerenciarAcompanhamento($produtos, GerenciarAcompanhamento::CRIAR_ACOMPANHAMENTO));
     }
 };
