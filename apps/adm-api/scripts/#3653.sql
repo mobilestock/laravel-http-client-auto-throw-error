d578e6ffef9fd1921c822b08b20aa53a06731cd7
felprangel <feliperangel897@gmail.com>
2024-07-17 12:40:37 -0300

    adiciona update nos produtos em liquidacao

--- apps/adm-api/scripts/#3653.sql
+++ apps/adm-api/scripts/#3653.sql
@@ -1,2 +1,7 @@
  ALTER TABLE produtos
    ADD COLUMN em_liquidacao TINYINT(1) NOT NULL DEFAULT '0';
+
+UPDATE produtos
+INNER JOIN catalogo_fixo ON catalogo_fixo.id_produto = produtos.id
+SET produtos.em_liquidacao = 1
+WHERE catalogo_fixo.tipo = 'LIQUIDACAO';
