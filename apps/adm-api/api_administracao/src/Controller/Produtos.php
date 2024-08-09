b2da12b645d29df427fac0a6247e34d750336d3c
GuilhermeABRoncari <guilhermeabroncari@hotmail.com>
2024-08-09 16:57:25 -0300

    Formata sku para delph

--- apps/adm-api/api_administracao/src/Controller/Produtos.php
+++ apps/adm-api/api_administracao/src/Controller/Produtos.php
@@ -913,7 +913,7 @@ class Produtos extends Request_m
                         'nome_tamanho' => $produtoSku->nome_tamanho,
                         'referencia' => $produto->descricao . ' ' . $produto->cores,
                         'sku_qrcode' => 'SKU' . $produtoSku->sku,
-                        'sku' => $produtoSku->sku,
+                        'sku' => implode('-', mb_str_split($produtoSku->sku, 4)),
                     ];
                 }
             }
