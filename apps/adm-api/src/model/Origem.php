ebf3064dec892e8044034b92d0af8032a71a9c7a
Roberto Griel Filho <roberto@griel.com.br>
2024-08-09 18:58:11 -0300

    Alterando o nome de origem dos aplicativos

--- apps/adm-api/src/model/Origem.php
+++ apps/adm-api/src/model/Origem.php
@@ -39,10 +39,10 @@ class Origem
             case 'meulook':
                 $this->valor = self::ML;
                 return;
-            case 'app-mobile-interno':
+            case 'app-interno':
                 $this->valor = self::APLICATIVO_INTERNO;
                 return;
-            case 'appEntregas':
+            case 'app-entrega':
                 $this->valor = self::APLICATIVO_ENTREGAS;
                 return;
         }
