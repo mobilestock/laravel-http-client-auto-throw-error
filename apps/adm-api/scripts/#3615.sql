a85a0cbf28ab654e548b60570e26d8d83a62fc03
MobileWorker1st <mobileworker2024@gmail.com>
2024-08-27 18:00:20 -0300

    Corrigindo script

--- apps/adm-api/scripts/#3615.sql
+++ apps/adm-api/scripts/#3615.sql
@@ -1,4 +1,4 @@
-DELETE FROM produtos_pontuacoes WHERE produtos_pontuacoes.id_produto NOT IN (produtos.id);
+DELETE FROM produtos_pontuacoes WHERE produtos_pontuacoes.id_produto NOT IN (SELECT produtos.id FROM produtos);
 
 ALTER TABLE produtos_pontuacoes
 ADD FOREIGN KEY (id_produto) REFERENCES produtos(id)
