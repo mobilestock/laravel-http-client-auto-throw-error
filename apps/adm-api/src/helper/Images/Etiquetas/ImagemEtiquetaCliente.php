c32dad29ad8671bb4d4335d08cbdb393d9d2f7a4
felprangel <feliperangel897@gmail.com>
2024-08-29 09:43:26 -0300

    ajusta altura da previsão

--- apps/adm-api/src/helper/Images/Etiquetas/ImagemEtiquetaCliente.php
+++ apps/adm-api/src/helper/Images/Etiquetas/ImagemEtiquetaCliente.php
@@ -181,7 +181,7 @@ class ImagemEtiquetaCliente extends ImagemAbstrata
         }
 
         if ($this->previsao) {
-            $this->texto($etiqueta, 16, 170, 115, $this->previsao);
+            $this->texto($etiqueta, 16, 170, 165, $this->previsao);
         }
         return $etiqueta;
     }
