62efc47d5f097654aea24cccefd923d8378e85ba
SavioAmin <87980098+SavioSReis@users.noreply.github.com>
2024-08-29 18:19:28 -0300

    Cast de dados

---
+++ apps/med-api/config/pdo-interceptor.php
@@ -0,0 +1,22 @@
+<?php
+
+use MobileStock\Shared\PdoInterceptor\Middlewares\CastWithDatabaseColumns;
+
+return [
+    'middlewares' => fn() => [
+        new CastWithDatabaseColumns([
+            'tem',
+            'eh',
+            'pode',
+            'falta',
+            'esta',
+            'ja',
+            'possui',
+            'permite',
+            'deve',
+            'existe',
+            'afetou',
+            'em',
+        ]),
+    ],
+];
