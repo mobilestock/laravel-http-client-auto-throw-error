438ea05dea048f07bb36ecb9c1f86051482b2767
SavioAmin <87980098+SavioSReis@users.noreply.github.com>
2024-08-29 18:19:16 -0300

    Consumindo da shared

--- apps/med-api/config/app.php
+++ apps/med-api/config/app.php
@@ -1,6 +1,8 @@
 <?php
 
+use Illuminate\Auth\AuthServiceProvider;
 use Illuminate\Support\Facades\Facade;
+use MobileStock\Shared\SharedServiceProvider;
 
 return [
     /*
@@ -40,7 +42,7 @@ return [
     |
     */
 
-    'debug' => (bool) env('APP_DEBUG', false),
+    'debug' => env('APP_DEBUG', false),
 
     /*
     |--------------------------------------------------------------------------
@@ -139,7 +141,6 @@ return [
 
     'maintenance' => [
         'driver' => 'file',
-        // 'store'  => 'redis',
     ],
 
     /*
@@ -154,40 +155,17 @@ return [
     */
 
     'providers' => [
-        /*
-         * Laravel Framework Service Providers...
-         */
-        Illuminate\Auth\AuthServiceProvider::class,
-        //        Illuminate\Broadcasting\BroadcastServiceProvider::class,
-        //        Illuminate\Bus\BusServiceProvider::class,
-        Illuminate\Cache\CacheServiceProvider::class,
-        Illuminate\Foundation\Providers\ConsoleSupportServiceProvider::class,
-        //        Illuminate\Cookie\CookieServiceProvider::class,
-        Illuminate\Database\DatabaseServiceProvider::class,
-        //        Illuminate\Encryption\EncryptionServiceProvider::class,
-        Illuminate\Filesystem\FilesystemServiceProvider::class,
+        SharedServiceProvider::class,
+        AuthServiceProvider::class,
+
         Illuminate\Foundation\Providers\FoundationServiceProvider::class,
-        Illuminate\Hashing\HashServiceProvider::class,
-        //        Illuminate\Notifications\NotificationServiceProvider::class,
-        //        Illuminate\Pagination\PaginationServiceProvider::class,
-        //        Illuminate\Pipeline\PipelineServiceProvider::class,
         Illuminate\Queue\QueueServiceProvider::class,
-        Illuminate\Redis\RedisServiceProvider::class,
-        //        Illuminate\Auth\Passwords\PasswordResetServiceProvider::class,
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
+        Illuminate\Database\DatabaseServiceProvider::class,
+        Illuminate\Redis\RedisServiceProvider::class,
 
-        /*
-         * Package Service Providers...
-         */
-
-        /*
-         * Application Service Providers...
-         */
         App\Providers\AppServiceProvider::class,
-        //        App\Providers\AuthServiceProvider::class,
-        // App\Providers\BroadcastServiceProvider::class,
-        //        App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
     ],
 
