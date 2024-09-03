94fdebd2dad24b889696847dad3f4b38284fae97
SavioAmin <87980098+SavioSReis@users.noreply.github.com>
2024-09-03 13:21:55 -0300

    Script adiciona telefone na loja

---
+++ apps/med-api/database/migrations/2024_08_23_161815_3543.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    public function up(): void
+    {
+        Schema::table('lojas', function (Blueprint $tabela) {
+            $tabela->char('telefone', 11)->after('id_revendedor');
+        });
+
+        $bancoDados = env('DB_DATABASE_MOBILE_STOCK');
+        DB::update(
+            "UPDATE lojas
+            INNER JOIN $bancoDados.colaboradores ON $bancoDados.colaboradores.id = lojas.id_revendedor
+            SET lojas.telefone = $bancoDados.colaboradores.telefone
+            WHERE TRUE;"
+        );
+    }
+};
