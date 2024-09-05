7e2b6a5301dfd18721a1bad586c1d30139e64aa3
felprangel <feliperangel897@gmail.com>
2024-09-05 08:22:22 -0300

    remove issue

--- apps/adm-api/api_estoque/src/Controller/Separacao.php
+++ apps/adm-api/api_estoque/src/Controller/Separacao.php
@@ -64,9 +64,6 @@ class Separacao
     public function buscaEtiquetasParaSeparacao()
     {
         $dados = Request::all();
-        /**
-         * @issue https://github.com/mobilestock/backend/issues/543
-         */
         Validador::validar($dados, [
             'uuids' => [Validador::OBRIGATORIO, Validador::ARRAY, Validador::TAMANHO_MINIMO(1)],
             'tipo_etiqueta' => [
