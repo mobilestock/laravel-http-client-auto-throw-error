f70f934417d9f35d5e8d67a705b9de73b612711b
felprangel <feliperangel897@gmail.com>
2024-09-23 10:18:44 -0300

    INDENTAÇÃO

--- apps/adm-api/src/helper/PriceHandler.php
+++ apps/adm-api/src/helper/PriceHandler.php
@@ -7,7 +7,6 @@ use PDO;
 
 class PriceHandler
 {
-
     // public function __construct()
     // {
     // }
@@ -17,10 +16,10 @@ class PriceHandler
     //     return 660;
     // }
 
-    /** * Função para calcular o preço de venda do produto 
-     * @example Entrada: (100, 25) - Saída: 133.33 
-     * @param Float $valor: preço de custo do produto a ser calculado * 
-     * @param Float $porcentagem: porcentagem de comissao do produto * 
+    /** * Função para calcular o preço de venda do produto
+     * @example Entrada: (100, 25) - Saída: 133.33
+     * @param Float $valor: preço de custo do produto a ser calculado *
+     * @param Float $porcentagem: porcentagem de comissao do produto *
      * @return Float retorna o valor do produto + porcentagem */
     // public static function calculaValorVenda(float $valor, float $porcentagem): int
     // {
@@ -31,8 +30,8 @@ class PriceHandler
 
     /** * Função para calcular o valor liquido
      * @example Entrada: (133.33, 25) - Saída: 100.00
-     * @param Float $valor: preço de custo do produto a ser calculado * 
-     * @param Float $porcentagem: porcentagem de comissao do produto * 
+     * @param Float $valor: preço de custo do produto a ser calculado *
+     * @param Float $porcentagem: porcentagem de comissao do produto *
      * @return Float retorna o valor do produto + porcentagem */
     public static function calculaValorComissaoFornecedor(float $valor, float $porcentagem): float
     {
@@ -48,7 +47,7 @@ class PriceHandler
     //     //$sql = "SELECT retornavalorCalculadoCpfOuCnpj({$id_produto},{$id_cliente},0) preco";
     //     $sql = "SELECT
     //     CASE
-    //       WHEN colaboradores.regime = 1 
+    //       WHEN colaboradores.regime = 1
     //           THEN ( SELECT produtos.valor_venda_cnpj FROM produtos WHERE produtos.id = '{$id_produto}' )
     //           ELSE ( SELECT produtos.valor_venda_cpf FROM produtos WHERE produtos.id = '{$id_produto}' )
     //     END preco FROM colaboradores WHERE colaboradores.id = '{$id_cliente}'";
