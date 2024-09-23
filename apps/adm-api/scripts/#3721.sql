3aa88710ec43c7bf7f51e194e3b8424bb8a710a8
felprangel <feliperangel897@gmail.com>
2024-09-23 15:48:46 -0300

    adiciona drops de colunas

--- apps/adm-api/scripts/#3721.sql
+++ apps/adm-api/scripts/#3721.sql
@@ -1,8 +1,12 @@
 ALTER TABLE produtos
-    DROP COLUMN porcentagem_comissao;
+    DROP COLUMN porcentagem_comissao,
+    DROP COLUMN porcentagem_comissao_ml,
+    DROP COLUMN porcentagem_comissao_ms;
 
 ALTER TABLE configuracoes
-    DROP COLUMN porcentagem_comissao;
+    DROP COLUMN porcentagem_comissao,
+    DROP COLUMN porcentagem_comissao_ml,
+    DROP COLUMN porcentagem_comissao_ms;
 
 UPDATE configuracoes
     SET comissoes_json = '{"comissao_direito_coleta": 10, "produtos_json": {"porcentagem_comissao_ml": 11, "porcentagem_comissao_ms": 12.28, "custo_max_aplicar_taxa_ml": 60, "custo_max_aplicar_taxa_ms": 60, "taxa_produto_barato_ml": 2, "taxa_produto_barato_ms": 2}}';
