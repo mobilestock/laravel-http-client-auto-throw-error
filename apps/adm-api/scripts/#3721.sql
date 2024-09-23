cab577ed23e6cb16cbfa460f55c53a682f862d47
felprangel <feliperangel897@gmail.com>
2024-09-23 14:54:53 -0300

    edita trigger de produtos_before_insert

--- apps/adm-api/scripts/#3721.sql
+++ apps/adm-api/scripts/#3721.sql
@@ -6,3 +6,44 @@ ALTER TABLE configuracoes
 
 UPDATE configuracoes
     SET comissoes_json = '{"comissao_direito_coleta": 10, "produtos_json": {"porcentagem_comissao_ml": 11, "porcentagem_comissao_ms": 12.28, "custo_max_aplicar_taxa_ml": 60, "custo_max_aplicar_taxa_ms": 60, "taxa_produto_barato_ml": 2, "taxa_produto_barato_ms": 2}}';
+
+DROP TRIGGER IF EXISTS produtos_before_insert;
+
+DELIMITER //
+CREATE TRIGGER `produtos_before_insert` BEFORE INSERT ON `produtos` FOR EACH ROW BEGIN
+	DECLARE
+		VALOR_CALCULO_PORCENTAGEM_,
+		COMISSAO_MS,
+		COMISSAO_PONTO_COLETA,
+		COMISSAO_ML
+	DECIMAL(10,2) DEFAULT 0;
+
+	SET NEW.proporcao_caixa = 1;
+	SELECT
+		configuracoes.porcentagem_comissao_ms,
+		configuracoes.porcentagem_comissao_ponto_coleta,
+        configuracoes.porcentagem_comissao_ml
+		INTO
+			COMISSAO_MS,
+			COMISSAO_PONTO_COLETA,
+			COMISSAO_ML
+	FROM configuracoes
+	LIMIT 1;
+
+	SET NEW.porcentagem_comissao_ponto_coleta = COMISSAO_PONTO_COLETA;
+
+	IF(NEW.valor_custo_produto > 0)THEN
+
+		IF(NEW.preco_promocao > 0 AND NEW.preco_promocao <= 100 ) THEN
+			SET VALOR_CALCULO_PORCENTAGEM_ = NEW.valor_custo_produto - ( NEW.valor_custo_produto * ( NEW.preco_promocao / 100 ) );
+		ELSE
+			SET VALOR_CALCULO_PORCENTAGEM_ = NEW.valor_custo_produto;
+		END IF;
+
+		SET NEW.valor_venda_ms = VALOR_CALCULO_PORCENTAGEM_ * ( 1 + ( COMISSAO_MS / ( 100 - COMISSAO_MS ) ) ),
+			 NEW.valor_venda_ml = VALOR_CALCULO_PORCENTAGEM_ + ROUND(VALOR_CALCULO_PORCENTAGEM_ * COMISSAO_ML / 100, 2)
+															 + ROUND(VALOR_CALCULO_PORCENTAGEM_ * NEW.porcentagem_comissao_ponto_coleta / 100, 2),
+			 NEW.valor_venda_sem_comissao = VALOR_CALCULO_PORCENTAGEM_;
+	END IF;
+END//
+DELIMITER ;
