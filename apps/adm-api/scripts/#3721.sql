ea401364cac2e576da6477501e794d3caf1a0f41
felprangel <feliperangel897@gmail.com>
2024-09-23 12:49:52 -0300

    altera valor da taxa

--- apps/adm-api/scripts/#3721.sql
+++ apps/adm-api/scripts/#3721.sql
@@ -5,4 +5,4 @@ ALTER TABLE configuracoes
     DROP COLUMN porcentagem_comissao;
 
 UPDATE configuracoes
-    SET comissoes_json = '{"comissao_direito_coleta": 10, "produtos_json": {"porcentagem_comissao_ml": 11, "porcentagem_comissao_ms": 12.28, "custo_max_aplicar_taxa_ml": 60, "custo_max_aplicar_taxa_ms": 60, "taxa_produto_barato_ml": 1.5, "taxa_produto_barato_ms": 1.5}}';
+    SET comissoes_json = '{"comissao_direito_coleta": 10, "produtos_json": {"porcentagem_comissao_ml": 11, "porcentagem_comissao_ms": 12.28, "custo_max_aplicar_taxa_ml": 60, "custo_max_aplicar_taxa_ms": 60, "taxa_produto_barato_ml": 2, "taxa_produto_barato_ms": 2}}';
