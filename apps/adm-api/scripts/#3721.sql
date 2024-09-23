22ce488dc000e11db4d008568f83c446d295edee
felprangel <feliperangel897@gmail.com>
2024-09-23 15:14:59 -0300

    dropa trigger

--- apps/adm-api/scripts/#3721.sql
+++ apps/adm-api/scripts/#3721.sql
@@ -8,6 +8,7 @@ UPDATE configuracoes
     SET comissoes_json = '{"comissao_direito_coleta": 10, "produtos_json": {"porcentagem_comissao_ml": 11, "porcentagem_comissao_ms": 12.28, "custo_max_aplicar_taxa_ml": 60, "custo_max_aplicar_taxa_ms": 60, "taxa_produto_barato_ml": 2, "taxa_produto_barato_ms": 2}}';
 
 DROP TRIGGER IF EXISTS produtos_before_insert;
+DROP TRIGGER IF EXISTS produtos_before_update;
 
 DELIMITER //
 
