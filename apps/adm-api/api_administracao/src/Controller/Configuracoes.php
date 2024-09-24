7935cdaf31c4a98e1f506255ab64a88d623cbb6f
felprangel <feliperangel897@gmail.com>
2024-09-24 14:54:04 -0300

    renomeia funções

--- apps/adm-api/api_administracao/src/Controller/Configuracoes.php
+++ apps/adm-api/api_administracao/src/Controller/Configuracoes.php
@@ -27,7 +27,7 @@ class Configuracoes extends Request_m
         return $comissoes;
     }
 
-    public function alteraComissoes()
+    public function alteraPorcentagemComissoes()
     {
         $dadosJson = FacadesRequest::all();
         Validador::validar($dadosJson, [
@@ -36,7 +36,7 @@ class Configuracoes extends Request_m
             'comissao_ponto_coleta' => [Validador::OBRIGATORIO, Validador::NUMERO],
         ]);
 
-        ConfiguracaoService::alteraComissoes($dadosJson);
+        ConfiguracaoService::alteraPorcentagemComissoes($dadosJson);
     }
 
     // public function atualizaAlertaChatAtendimento()
