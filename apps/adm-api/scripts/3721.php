17f737e529a08e71b684bc35b839c3e936262818
felprangel <feliperangel897@gmail.com>
2024-09-25 14:25:59 -0300

    adiciona issue

--- apps/adm-api/scripts/3721.php
+++ apps/adm-api/scripts/3721.php
@@ -7,6 +7,9 @@ use MobileStock\jobs\config\AbstractJob;
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
+/**
+ * @issue https://github.com/mobilestock/backend/issues/581
+ */
 return new class extends AbstractJob {
     public const TAXA_PRODUTO_BARATO = 2;
     public const CUSTO_MAXIMO_APLICAR_TAXA = 60;
