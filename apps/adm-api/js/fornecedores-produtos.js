ae7472854b581675f15c8aecf8c5819f6c91daca
felprangel <feliperangel897@gmail.com>
2024-09-25 09:19:10 -0300

    adiciona cálculo para valor prdduto

--- apps/adm-api/js/fornecedores-produtos.js
+++ apps/adm-api/js/fornecedores-produtos.js
@@ -349,6 +349,16 @@ var fornecedoresProdutosVUE = new Vue({
       }
       this.formulario.valor_venda_ms = (custo * (1 + this.calculoVolta(this.porcentagemMS.valor))).toFixed(2)
       this.formulario.valor_venda_ml = (custo * (1 + this.porcentagemML.valor / 100)).toFixed(2)
+      if (this.formulario.valor_custo_produto < this.porcentagemMS.custo_max_aplicar_taxa_ms) {
+        this.formulario.valor_venda_ms = (
+          Number(this.formulario.valor_venda_ms) + Number(this.porcentagemMS.taxa_produto_barato_ms)
+        ).toFixed(2)
+      }
+      if (this.formulario.valor_custo_produto < this.porcentagemML.custo_max_aplicar_taxa_ml) {
+        this.formulario.valor_venda_ml = (
+          Number(this.formulario.valor_venda_ml) + Number(this.porcentagemML.taxa_produto_barato_ml)
+        ).toFixed(2)
+      }
       this.formulario.valor_custo_produto = custo
     },
     // -------------AJAXS---------------------
