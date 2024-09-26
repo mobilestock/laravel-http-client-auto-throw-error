77c4500faf112fd56d0c11c75d815a29feabfd80
felprangel <feliperangel897@gmail.com>
2024-09-26 09:37:38 -0300

    adiciona função no tools

--- apps/design-system/storybook-web/packages/base/tools/index.ts
+++ apps/design-system/storybook-web/packages/base/tools/index.ts
@@ -3,6 +3,16 @@ import { BaseSyntheticEvent } from "react"
 export default {
   imageOnError(event: BaseSyntheticEvent<Event, EventTarget & HTMLImageElement>): void {
       event.target.src = '/resources/images/broken-image.png'
-  }
+  },
+  sanitizaString(texto: string): string {
+    const textoFormatado = texto
+      .normalize('NFD')
+      .replace(/[\u0300-\u036f]/g, '')
+      .replace(/\-\-+/g, '')
+      .replace(/(^-+|-+$)/, '')
+      .replace(/[^a-z\s]/gi, '')
+
+    return textoFormatado
+  },
 }
 
