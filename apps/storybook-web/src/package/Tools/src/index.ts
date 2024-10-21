d9492f2750b8a44fa22a3c1639485494b2b13d59
GuilhermeABRoncari <guilhermeabroncari@hotmail.com>
2024-10-21 16:01:46 -0300

    Inicia alterações nos componentes

---
+++ apps/storybook-web/src/package/Tools/src/index.ts
@@ -0,0 +1,20 @@
+import { BaseSyntheticEvent } from 'react'
+
+const tools = {
+  imageOnError(event: BaseSyntheticEvent<Event, EventTarget & HTMLImageElement>): void {
+    event.target.src = '/resources/images/broken-image.png'
+  },
+
+  sanitizeString(text: string): string {
+    const sanitizedText = text
+      .normalize('NFD')
+      .replace(/[\u0300-\u036f]/g, '')
+      .replace(/\-\-+/g, '')
+      .replace(/(^-+|-+$)/, '')
+      .replace(/[^a-z\s]/gi, '')
+
+    return sanitizedText
+  }
+}
+
+export default tools
