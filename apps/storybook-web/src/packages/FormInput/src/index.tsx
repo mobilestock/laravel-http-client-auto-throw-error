b54417161c8f627d4be334fd924576882b168bd2
GuilhermeABRoncari <guilhermeabroncari@hotmail.com>
2024-10-22 14:37:12 -0300

    Altera arquivos de empacotamento

--- apps/storybook-web/src/packages/FormInput/src/index.tsx
+++ apps/storybook-web/src/packages/FormInput/src/index.tsx
@@ -2,7 +2,7 @@ import { useEffect, useRef } from 'react'
 
 import { useField } from '@unform/core'
 
-import Input, { InputProps } from '@mobilestock/input'
+import Input, { InputProps } from '@mobilestock-web/input'
 
 interface FormInputProps extends InputProps {
   name: string
