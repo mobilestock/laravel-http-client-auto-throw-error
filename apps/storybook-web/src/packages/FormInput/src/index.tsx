fbb6597f91a8b3053e72e19f390f4521ce011e23
GuilhermeABRoncari <guilhermeabroncari@hotmail.com>
2024-10-22 11:01:34 -0300

    Altera estrutura de pacotes

---
+++ apps/storybook-web/src/packages/FormInput/src/index.tsx
@@ -0,0 +1,30 @@
+import { useEffect, useRef } from 'react'
+
+import { useField } from '@unform/core'
+
+import Input, { InputProps } from '@mobilestock/input'
+
+interface FormInputProps extends InputProps {
+  name: string
+}
+
+export default function FormInput(props: FormInputProps): JSX.Element {
+  const { fieldName, defaultValue, registerField, error } = useField(props.name)
+  const inputRef = useRef<HTMLInputElement>(null)
+
+  useEffect(() => {
+    registerField({
+      name: fieldName,
+      ref: inputRef,
+      getValue: ref => ref.current.value,
+      setValue: (ref, value) => {
+        ref.current.value = value
+      },
+      clearValue: ref => {
+        ref.current.value = ''
+      }
+    })
+  }, [fieldName, registerField])
+
+  return <Input ref={inputRef} defaultValue={defaultValue} error={error} {...props} />
+}
