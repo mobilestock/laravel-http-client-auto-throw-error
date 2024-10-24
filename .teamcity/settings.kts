223ecf863412096bf48ce02ac74df867c0a175bc
GuilhermeABRoncari <guilhermeabroncari@hotmail.com>
2024-10-24 09:45:00 -0300

    Retorna arquivo a forma original

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -145,35 +145,143 @@ object Deploy : BuildType({
 
     steps {
         script {
-            name = "teste"
-            id = "teste"
-            scriptContent = "ls -la"
+            name = "ECR Login"
+            id = "login"
+            scriptContent = "docker run --rm -e AWS_ACCESS_KEY_ID=%env.AWS_ACCESS_KEY_ID% -e AWS_SECRET_ACCESS_KEY=%env.AWS_SECRET_ACCESS_KEY% amazon/aws-cli ecr get-login-password --region sa-east-1 | docker login --username AWS --password-stdin %env.CONTAINER_REGISTRY%"
         }
-        // dockerCommand {
-        //     name = "Run Docker Build"
-        //     id = "run_docker_build"
-        //     commandType = build {
-        //         source = file {
-        //             path = "Dockerfile"
-        //         }
-        //         namesAndTags = "autopublish-image"
-        //     }
-        // }
-        // dockerCommand {
-        //     name = "Run Docker Run"
-        //     id = "run_docker_run"
-        //     commandType = other {
-        //         subCommand = "run"
-        //         commandArgs = """
-        //           --rm \
-        //           -e NPM_TOKEN=%env.NPM_TOKEN% \
-        //           -v $(pwd)/scripts/autoPublish:/app/scripts/autoPublish \
-        //           -v $(pwd)/apps/storybook-native/src/packages:/app/apps/storybook-native/src/packages \
-        //           -v $(pwd)/apps/storybook-web/src/packages:/app/apps/storybook-web/src/packages \
-        //           autopublish-image
-        //         """.trimIndent()
-        //     }
-        // }
+        script {
+            name = "[build] backend-shared"
+            id = "build_1"
+            scriptContent = "docker build --platform linux/amd64 -t backend-shared:latest ./shared"
+        }
+        dockerCommand {
+            name = "[build] adm-api"
+            id = "adm_api"
+            commandType = build {
+                source = file {
+                    path = "apps/adm-api/Dockerfile"
+                }
+                namesAndTags = "%env.CONTAINER_REGISTRY%adm-api:latest"
+                commandArgs = "--platform linux/amd64"
+            }
+        }
+        dockerCommand {
+            name = "[push] adm-api"
+            id = "push"
+            commandType = push {
+                namesAndTags = "%env.CONTAINER_REGISTRY%adm-api"
+            }
+        }
+        dockerCommand {
+            name = "[build] adm-cli"
+            id = "build_adm-cli"
+            commandType = build {
+                source = file {
+                    path = "apps/adm-api/Dockerfile.cli"
+                }
+                namesAndTags = "%env.CONTAINER_REGISTRY%adm-cli:latest"
+                commandArgs = "--platform linux/amd64"
+            }
+        }
+        dockerCommand {
+            name = "[push] adm-cli"
+            id = "push_adm-cli"
+            commandType = push {
+                namesAndTags = "%env.CONTAINER_REGISTRY%adm-cli"
+            }
+        }
+        dockerCommand {
+            name = "[build] lookpay-api"
+            id = "build_lookpay_api"
+            commandType = build {
+                source = file {
+                    path = "apps/lookpay-api/Dockerfile"
+                }
+                namesAndTags = """
+                    lookpay-api:latest
+                    %env.CONTAINER_REGISTRY%lookpay-api:latest
+                """.trimIndent()
+                commandArgs = "--platform linux/amd64"
+            }
+        }
+        dockerCommand {
+            name = "[push] lookpay-api"
+            id = "push_lookpay_api"
+            commandType = push {
+                namesAndTags = "%env.CONTAINER_REGISTRY%lookpay-api:latest"
+                removeImageAfterPush = false
+            }
+        }
+        dockerCommand {
+            name = "[build] med-api"
+            id = "build_med_api"
+            commandType = build {
+                source = file {
+                    path = "apps/med-api/Dockerfile"
+                }
+                namesAndTags = """
+                    med-api:latest
+                    %env.CONTAINER_REGISTRY%med-api:latest
+                """.trimIndent()
+                commandArgs = "--platform linux/amd64"
+            }
+        }
+        dockerCommand {
+            name = "[push] med-api"
+            id = "push_med_api"
+            commandType = push {
+                namesAndTags = "%env.CONTAINER_REGISTRY%med-api:latest"
+                removeImageAfterPush = false
+            }
+        }
+        dockerCommand {
+            name = "[build] load-balancer"
+            id = "build_load_balancer"
+            commandType = build {
+                source = file {
+                    path = "apps/load-balancer/Dockerfile"
+                }
+                namesAndTags = "%env.CONTAINER_REGISTRY%load-balancer:latest"
+                commandArgs = "--platform linux/amd64"
+            }
+        }
+        dockerCommand {
+            name = "[push] load-balancer"
+            id = "push_load_balancer"
+            commandType = push {
+                namesAndTags = "%env.CONTAINER_REGISTRY%load-balancer:latest"
+            }
+        }
+        script {
+            name = "[Deploy] stack backend"
+            id = "deploy"
+            scriptContent = "curl -X POST https://portainer.lookpay.com.br/api/stacks/webhooks/8b7f771f-1012-4d64-bc7f-9b9a68c911c8"
+            formatStderrAsError = true
+        }
+        script {
+            name = "[Deploy] adm-cli-sqs"
+            id = "deploy-service-1"
+            scriptContent = "curl -X POST https://portainer.lookpay.com.br/api/webhooks/22ac410d-1418-48eb-b580-a0021b3a1ae2"
+            formatStderrAsError = true
+        }
+        script {
+            name = "[Deploy] adm-cli-job-atualiza-sicoob"
+            id = "deploy-service-2"
+            scriptContent = "curl -X POST https://portainer.lookpay.com.br/api/webhooks/e1d5b840-1e30-4abf-8335-f0887a4e8434"
+            formatStderrAsError = true
+        }
+        script {
+            name = "[Deploy] Notification"
+            id = "notification"
+            executionMode = BuildStep.ExecutionMode.RUN_ONLY_ON_FAILURE
+            scriptContent = """
+                #!/bin/bash
+
+                MESSAGE="#TEAM_CITY_BUILD_ERROR\n\nO build %system.build.number% do projeto %system.teamcity.projectName% falhou ao tentar fazer o deploy. Detalhes: %env.BUILD_URL%"
+
+                curl -X POST -H 'Content-Type: application/json' -d "{\"chat_id\": \"%env.TELEGRAM_CHAT_ID%\", \"text\": \"${'$'}MESSAGE\", \"disable_notification\": true}" https://api.telegram.org/bot%env.TELEGRAM_BOT_TOKEN%/sendMessage
+            """.trimIndent()
+        }
     }
 
     triggers {
