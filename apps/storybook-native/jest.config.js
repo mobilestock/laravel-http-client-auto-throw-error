08074648e53ef441264f3b4d99c3b5a506ffd9da
GuilhermeABRoncari <guilhermeabroncari@hotmail.com>
2024-10-31 10:46:58 -0300

    Remove arquivo desnecessário

--- apps/storybook-native/jest.config.js
+++ apps/storybook-native/jest.config.js
@@ -6,13 +6,13 @@ const mocksDir = path.resolve(__dirname, '__mocks__')
 const getMockFiles = () => {
   return fs
     .readdirSync(mocksDir)
-    .filter(file => file.endsWith('.js') || file.endsWith('.tsx'))
-    .map(file => path.join(mocksDir, file))
+    .filter((file) => file.endsWith('.tsx'))
+    .map((file) => path.join(mocksDir, file))
 }
 
 module.exports = {
   preset: 'react-native',
   setupFilesAfterEnv: ['@testing-library/jest-native/extend-expect'],
   transformIgnorePatterns: ['node_modules/(?!(react-native|@react-native|@testing-library|styled-components)/)'],
-  setupFiles: getMockFiles()
+  setupFiles: getMockFiles(),
 }
