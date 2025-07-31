8af67e549c677309aad07b81c87de9ce1306f371
geangontijo <geangontijo07@gmail.com>
2025-07-31 15:26:13 -0300

    refactor(http-client-auto-throw-error): adiciona tipagem de retorno no service provider
    Adiciona a tipagem de retorno `void` ao método `boot` e `Response` à
    closure do listener de evento.
    
    Essa alteração melhora a legibilidade e a robustez do código,
    alinhando-o com as boas práticas de tipagem estrita do PHP.

--- src/HttpClientAutoThrowErrorServiceProvider.php
+++ src/HttpClientAutoThrowErrorServiceProvider.php
@@ -4,14 +4,15 @@ namespace MobileStock\LaravelHttpClientAutoThrowError;
 
 use Illuminate\Http\Client\Events\ResponseReceived;
 use Illuminate\Http\Client\RequestException;
+use Illuminate\Http\Client\Response;
 use Illuminate\Support\Facades\Event;
 use Illuminate\Support\ServiceProvider;
 
 class HttpClientAutoThrowErrorServiceProvider extends ServiceProvider
 {
-    public function boot()
+    public function boot(): void
     {
-        Event::listen(fn(ResponseReceived $event) => $event->response->throw());
+        Event::listen(fn(ResponseReceived $event): Response => $event->response->throw());
         RequestException::dontTruncate();
     }
 }
